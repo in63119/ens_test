ab2b11bb70429afdedd0a35cd69c07ec4bb48ab3
이인수 <in63119@gmail.com>
2023-09-12 15:45:36 +0900

    mui init setting

--- front-ens/src/pages/Main.js
+++ front-ens/src/pages/Main.js
@@ -1,7 +1,10 @@
 import React from "react";
 
+// MUI css
+import { Box } from "@mui/material";
+
 const Main = () => {
-  return <div>여긴 메인 페이지 입니다.</div>;
+  return <Box>여긴 메인 페이지 입니다.</Box>;
 };
 
 export default Main;
